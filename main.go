c02038892bbefb567e500e9cb6311abdc5ed8064
Risman Kurniadi <rismankurniadi2@gmail.com>
2022-10-12 21:59:47 +0700

    Push final

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"Assignment3-AutoReload/helper"
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	go helper.CreateJson()
+	http.HandleFunc("/", helper.ReloadWeb)
+	fmt.Println("Server aktif di http://127.0.0.1:9999")
+	fmt.Println("Keluar Server CTRL+ C")
+	http.ListenAndServe(":9999", nil)
 }
